443c1bc81a3dd340dd411e4a5bb2232484cb15de
Elizabeth <gammon.jocular.0w@icloud.com>
2024-04-12 19:03:24 +0100

    9838
    Added zoom mode & max width

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -623,6 +623,7 @@ impl Default for StatusLineConfig {
             ],
             center: vec![],
             right: vec![
+                E::Zoom,
                 E::Diagnostics,
                 E::Selections,
                 E::Register,
@@ -726,6 +727,9 @@ pub enum StatusLineElement {
 
     /// The base of current working directory
     CurrentWorkingDirectory,
+
+    /// Current zoom/zen state
+    Zoom,
 }
 
 // Cursor shape is read and used on every rendered frame and so needs
