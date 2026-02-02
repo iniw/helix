3a83f418ea751dc0d410ffe6459aa475c559c7a8
Austen Adler <stonewareslord@gmail.com>
2026-02-02 08:21:23 -0300

    15199
    action: paste_before_all, paste_after_all
    
    separate selections with newlines
    
    Co-authored-by: Axlefublr <axlefublr.ls@gmail.com>

--- helix-term/src/commands/typed.rs
+++ helix-term/src/commands/typed.rs
@@ -1188,7 +1188,7 @@ fn paste_clipboard_after(
         return Ok(());
     }
 
-    paste(cx.editor, '+', Paste::After, 1);
+    paste(cx.editor, '+', Paste::After, 1, PasteType::Default);
     Ok(())
 }
 
@@ -1201,7 +1201,7 @@ fn paste_clipboard_before(
         return Ok(());
     }
 
-    paste(cx.editor, '+', Paste::Before, 1);
+    paste(cx.editor, '+', Paste::Before, 1, PasteType::Default);
     Ok(())
 }
 
@@ -1214,7 +1214,7 @@ fn paste_primary_clipboard_after(
         return Ok(());
     }
 
-    paste(cx.editor, '*', Paste::After, 1);
+    paste(cx.editor, '*', Paste::After, 1, PasteType::Default);
     Ok(())
 }
 
@@ -1227,7 +1227,7 @@ fn paste_primary_clipboard_before(
         return Ok(());
     }
 
-    paste(cx.editor, '*', Paste::Before, 1);
+    paste(cx.editor, '*', Paste::Before, 1, PasteType::Default);
     Ok(())
 }
 
