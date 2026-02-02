78b74ad3ec813130a172b3acd2a81308fa293333
Austen Adler <stonewareslord@gmail.com>
2026-02-02 08:21:23 -0300

    15199
    action: paste_before_all, paste_after_all
    
    separate selections with newlines
    
    Co-authored-by: Axlefublr <axlefublr.ls@gmail.com>

--- helix-term/src/ui/editor.rs
+++ helix-term/src/ui/editor.rs
@@ -1421,6 +1421,7 @@ impl EditorView {
                         config.mouse_yank_register,
                         commands::Paste::Before,
                         cxt.count(),
+                        commands::PasteType::Default,
                     );
 
                     return EventResult::Consumed(None);
