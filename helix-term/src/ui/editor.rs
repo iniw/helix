6041936cedf59efe5549629684aa5d63f7279040
Tobias Langendorf <junglerobba@jngl.one>
2026-02-18 13:29:14 -0300

    14481
    jobs: allow callbacks to return followup jobs
    
    This introduces `Callback::Followup` that can optionally return
    another job to be queued after the callback finishes.
    This allows creation of async operation chains where the editor state
    needs to be queried in between async operations, without needing
    blocking operations.
    
    Needed for features like running multiple LSP code actions in order,
    where each action must operate on the latest document state after the
    previous action completes.
    
    lsp: add code actions on save
    
    Adds a new `code-actions-on-save` config option for LSP code actions to
    run on save, before auto formatting will be applied.
    
    These actions will be spawned as a series of alternating async jobs and
    callbacks, using the callback followups from the previous commit, which
    starts the next async task after each callback executes.
    This is needed to ensure these actions each operate on the latest
    document version and will run in the configured order, so access to
    non-thread-safe Editor is required for querying document state in
    between async tasks.
    
    Because this is run automatically on save, and is user-configurable, the
    reasonable default is using each action from the first LSP that
    advertises it and, in case it resolves to multiple actions, use the
    first one. This way no user interaction is required, and potentially
    conflicting applications of actions are avoided.
    
    commands: skip code actions on auto save
    
    Enables the `--no-format` flag for the write and write-all commands to
    also disable configured on save code actions.
    
    Co-authored-by: Jonatan Pettersson <jonatan.pettersson@proton.me>

--- helix-term/src/ui/editor.rs
+++ helix-term/src/ui/editor.rs
@@ -1591,6 +1591,7 @@ impl Component for EditorView {
                         force: false,
                         write_scratch: false,
                         auto_format: false,
+                        code_actions: false,
                     };
                     if let Err(e) = commands::typed::write_all_impl(context, options) {
                         context.editor.set_error(format!("{}", e));
