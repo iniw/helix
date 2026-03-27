7e450a1004258a2b2abe4ea99ae80257e5b20b51
Pascal Kuthe <pascalkuthe@pm.me>
2026-03-26 22:26:08 -0300

    14544
    implement file-watching based on filesentry
    
    automatically reload git repo when commit changes
    
    support for relative lsp file watcher patterns
    
    graceously degrade when filesentry is unavailable

--- helix-term/src/commands/typed.rs
+++ helix-term/src/commands/typed.rs
@@ -1555,10 +1555,12 @@ fn reload(cx: &mut compositor::Context, _args: Args, event: PromptEvent) -> anyh
         view.ensure_cursor_in_view(doc, scrolloff);
     })?;
     if let Some(path) = doc.path() {
-        cx.editor
-            .language_servers
-            .file_event_handler
-            .file_changed(path.clone());
+        if !cx.editor.file_watcher.is_watching(path) {
+            cx.editor
+                .language_servers
+                .file_event_handler
+                .file_changed(path.clone());
+        }
     }
     Ok(())
 }
@@ -1601,10 +1603,12 @@ fn reload_all(cx: &mut compositor::Context, _args: Args, event: PromptEvent) -> 
         }
 
         if let Some(path) = doc.path() {
-            cx.editor
-                .language_servers
-                .file_event_handler
-                .file_changed(path.clone());
+            if !cx.editor.file_watcher.is_watching(path) {
+                cx.editor
+                    .language_servers
+                    .file_event_handler
+                    .file_changed(path.clone());
+            }
         }
 
         for view_id in view_ids {
