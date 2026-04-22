60795d5e6c3cfcd06afd68102aa043f00bb8ae38
Vinicius Deolindo <git@vini.cat>
2026-04-21 21:16:21 -0300

    editor: add `workspace-diagnostics` to the default `statusline.right`

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -696,6 +696,7 @@ impl Default for StatusLineConfig {
             ],
             center: vec![],
             right: vec![
+                E::WorkspaceDiagnostics,
                 E::SearchPosition,
                 E::Zoom,
                 E::Diagnostics,
